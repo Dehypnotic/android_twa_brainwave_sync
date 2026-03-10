4c89eb70b417c2cbfbcba4d7ab054956b088b24d
dehypnotic <jegrmeg@outlook.com>
2026-03-10 20:38:57 +0000

    Enforce portrait orientation and configChanges
    Add android:configChanges="orientation|screenSize|keyboardHidden" to the MainActivity entry in AndroidManifest.xml and call setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) in MainActivity.onCreate (with ActivityInfo import). This forces portrait mode and prevents activity restarts on rotation/size/keyboard configuration changes for the Trusted Web Activity/LauncherActivity.

--- app/src/main/java/com/dehypnotic/brainwavesync/MainActivity.java
+++ app/src/main/java/com/dehypnotic/brainwavesync/MainActivity.java
@@ -1,12 +1,14 @@
 package com.dehypnotic.brainwavesync;
 
 import android.os.Bundle;
+import android.content.pm.ActivityInfo;
 import com.google.androidbrowserhelper.trusted.LauncherActivity;
 import androidx.browser.trusted.TrustedWebActivityDisplayMode;
 
 public class MainActivity extends LauncherActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
     }
 
