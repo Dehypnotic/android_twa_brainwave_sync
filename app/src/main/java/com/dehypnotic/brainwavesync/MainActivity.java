e33e0ea3c394ba3910b72cecbd4d2b7910b2458c
dehypnotic <jegrmeg@outlook.com>
2026-03-10 11:33:33 +0000

    Enable immersive TWA display and add theme colors
    Make the Trusted Web Activity immersive by overriding getDisplayMode in MainActivity to return an ImmersiveMode (sticky=true, layoutInDisplayCutoutMode=0) and add a basic onCreate override. Update Theme.BrainwaveSync to specify statusBarColor, navigationBarColor, and windowBackground using color resources to provide a consistent fullscreen appearance.

--- app/src/main/java/com/dehypnotic/brainwavesync/MainActivity.java
+++ app/src/main/java/com/dehypnotic/brainwavesync/MainActivity.java
@@ -1,9 +1,20 @@
 package com.dehypnotic.brainwavesync;
 
+import android.os.Bundle;
 import com.google.androidbrowserhelper.trusted.LauncherActivity;
+import androidx.browser.trusted.TrustedWebActivityDisplayMode;
 
 public class MainActivity extends LauncherActivity {
-  // TWA entry point.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected TrustedWebActivityDisplayMode getDisplayMode() {
+        return new TrustedWebActivityDisplayMode.ImmersiveMode(
+            true, // isSticky
+            0 // layoutInDisplayCutoutMode (0 = DEFAULT)
+        );
+    }
 }
-
-
